c57c970b5e3546ad4358526c25b0b951cbf3ee30
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-11-21 12:12:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let a= Number(prompt("Enter the 1st value: "))*2;
+	
+let b= Number(prompt("Enter the 2nd value: "));
+	const ans= a+b;
+
+alert("The final result is: "+ans);
